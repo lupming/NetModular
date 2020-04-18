ae97b56648433c1ed5ee1142fd257ea1a9f901c4
lupming <lupming@163.com>
2020-04-18 22:49:40 +0800

    取消Package生成

--- src/Framework/Data/Core/Data.Abstractions/Attributes/TableAttribute.cs
+++ src/Framework/Data/Core/Data.Abstractions/Attributes/TableAttribute.cs
@@ -13,6 +13,17 @@ namespace NetModular.Lib.Data.Abstractions.Attributes
         /// </summary>
         public string Name { get; set; }
 
+
+        /// <summary>
+        /// 是否多租户
+        /// </summary>
+        public bool IsTenant { get; set; }
+
+        /// <summary>
+        /// 租户编号对应数据库列名称
+        /// </summary>
+        public string TenantIdColumnName { get; set; }
+
         /// <summary>
         /// 指定实体类在数据库中对应的表名称
         /// </summary>
@@ -21,5 +32,19 @@ namespace NetModular.Lib.Data.Abstractions.Attributes
         {
             Name = tableName;
         }
+
+        /// <summary>
+        /// 指定实体类在数据库中对应的表名称
+        /// </summary>
+        /// <param name="tableName">表名</param>
+        /// <param name="isTenant">是否多租户</param>
+        /// <param name="tenantIdColumnName">租户编号对应数据库列名称</param>
+        public TableAttribute(string tableName, bool isTenant = false, string tenantIdColumnName = "TenantId")
+        {
+            Name = tableName;
+            IsTenant = isTenant;
+            TenantIdColumnName = tenantIdColumnName.NotNull() ? tenantIdColumnName : "TenantId";
+        }
+
     }
 }
---
+++ src/Framework/Host/Host.Web/Middleware/PathRewritingMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetModular.Lib.Host.Web.Middleware
+{
+    public class PathRewritingMiddleware
+    {
+
+        private readonly RequestDelegate _next;
+
+        private readonly string _domain;
+
+        private readonly string _path;
+
+
+        public PathRewritingMiddleware(RequestDelegate next, string domain, string path)
+        {
+            _next = next;
+
+            _domain = domain;
+
+            _path = path;
+        }
+
+        public Task Invoke(HttpContext context)
+        {
+            if (context.Request.Path.Equals("/") || context.Request.Path.Equals(""))
+            {
+                string host = context.Request.Host.Value;
+
+                if (host.Equals(_domain, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    context.Request.Path = _path;
+                }
+            }
+            return _next(context);
+        }
+    }
+}
